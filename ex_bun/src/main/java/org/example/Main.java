a9e7e55112ab19d5e5440a18f5dbae133fce00b3
cojocarucatalina <cojocarucatalina@yahoo.com>
2023-11-12 13:14:36 +0200

    Added cache and updates

--- ex_bun/src/main/java/org/example/Main.java
+++ ex_bun/src/main/java/org/example/Main.java
@@ -1,31 +1,63 @@
 package org.example;
 
 import database.DatabaseConnectionFactory;
+import database.JDBConnectionWrapper;
 import model.Book;
 import model.builder.BookBuilder;
 import repository.BookRepository;
+import repository.BookRepositoryCacheDecorator;
 import repository.BookRepositoryMySQL;
+import repository.Cache;
+import service.BookService;
+import service.BookServiceImpl;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
 
 
-        BookRepository bookRepository = new BookRepositoryMySQL(DatabaseConnectionFactory.getConnectionWrapper(true).getConnection());
+        BookRepository bookRepository = new BookRepositoryCacheDecorator(
+                new BookRepositoryMySQL(DatabaseConnectionFactory.getConnectionWrapper(true).getConnection()),
+                new Cache<>()
+        );
 
-        Book book = new BookBuilder()
+        BookService bookService = new BookServiceImpl(bookRepository);
+
+
+        Book book1 = new BookBuilder()
                 .setTitle("Harry Potter")
                 .setAuthor("J.K. Rowling")
                 .setPublishedDate(LocalDate.of(2010, 7, 3))
                 .build();
 
+        Book book2 = new BookBuilder()
+                .setTitle("Portretul lui Dorian Gray")
+                .setAuthor("Oscar Wilde")
+                .setPublishedDate(LocalDate.of(2010, 7, 3))
+                .build();
 
-        System.out.println(book);
-        System.out.println(bookRepository.findAll());
+        Book book3 = new BookBuilder()
+                .setTitle("Printre tonuri cenusii")
+                .setAuthor("Ruta Sepetys")
+                .setPublishedDate(LocalDate.of(2010, 7, 3))
+                .build();
 
 
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+
+        bookService.save(book1);
+        bookService.save(book2);
+        bookService.save(book3);
+
+        System.out.println(bookService.findAll());
+
+        System.out.println(bookService.findAll());
+        System.out.println(bookService.findAll());
 
 
 
